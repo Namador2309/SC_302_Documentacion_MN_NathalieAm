98edb986d8f9b0e75d4e470335abad72fd5c0f5c
Nath A. <natiamador789@gmail.com>
2025-01-26 16:12:50 -0600

    Update Mi-archivo.js

--- Mi-archivo.js
+++ Mi-archivo.js
@@ -2,5 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Other/javascript.js to edit this template
  */
+alert("hola mundo_editado")
+console.log("cambios en el archivo")
+
+//comentario de prueba
 
 
